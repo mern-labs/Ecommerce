03a0e4d1ea78978a320377ac6f498483b66a7297
GOWTHAM9384 <gowthamaravinth00@gmail.com>
2026-01-28 20:32:14 +0530

    add banner POST/GET

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -7,6 +7,8 @@ const app=express();
 app.use(express.json())
 app.use(cors())
 
+app.use("/uploads",express.static("uploads"))
+
 
 app.use("/api",bannerRoutes)
 
--- Backend/src/router/bannerRoute.js
+++ Backend/src/router/bannerRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bannerUploads = require("../middleware/bannerMulter");
-const { createBanner } = require("../controller/bannerController");
+const { createBanner, getBanner } = require("../controller/bannerController");
 const bannerRoutes = express.Router();
 
 bannerRoutes.post("/banners",bannerUploads.single("file"), createBanner);
+bannerRoutes.get("/banners", getBanner);
 
 
 module.exports = bannerRoutes;
