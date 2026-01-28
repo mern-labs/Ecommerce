93ce04129d8a195755f13f461125f80c03eb9142
GOWTHAM9384 <gowthamaravinth00@gmail.com>
2026-01-28 16:40:06 +0530

    add endpoint for banner

--- Frontend/src/interceptor/interceptor.js
+++ Frontend/src/interceptor/interceptor.js
@@ -1,8 +1,12 @@
-const axios=require('axios')
+import axios from "axios"
 
 const apiInstance=axios.create({
   basURL:"http://localhost:3000"
 })
 
 
-
+export const getBanner=async()=>{
+  const res=await apiInstance.get("/api/banners")
+  console.log(res.data);
+  return res
+}
--- Frontend/src/pages/Home.jsx
+++ Frontend/src/pages/Home.jsx
@@ -1,10 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../common/Navbar'
+import { getBanner } from '../interceptor/interceptor'
 
 const Home = () => {
+
+  const [banner, setBanner] = useState([])
+
+  useEffect(()=>{
+
+    const getBanners=async()=>{
+      try {
+        const res=await getBanner()
+        setBanner(res.data.banner)
+      } catch (error) {
+        console.log(error.message); 
+      }
+    }
+    getBanners()
+  },[])
   return (
     <div className=''>
       <h1>Hello</h1>
+
+      {banner.map((image,index)=>{
+        return <div key={index}>
+          <img src={image} alt="" />
+        </div>
+      })}
     </div>
   )
 }
