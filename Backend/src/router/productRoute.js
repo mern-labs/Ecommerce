f20d9cd37f650f2050bee1e4f1a56ce9675f183f
GOWTHAM9384 <gowthamaravinth00@gmail.com>
2026-01-31 13:34:37 +0530

    id based feching product

--- Backend/src/router/productRoute.js
+++ Backend/src/router/productRoute.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const productRoute = express.Router();
-const { createProduct, getProducts } = require("../controller/productController");
+const { createProduct, getProducts, getProductById } = require("../controller/productController");
 const productUploads = require("../middleware/productMulter");
 
 productRoute.post("/products",productUploads.single('file'), createProduct);
 productRoute.get("/products",getProducts);
+productRoute.get("/products/:id",getProductById);
 
 
 module.exports = productRoute;
+      
